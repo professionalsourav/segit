db81ddefe6a96db008292b8de35fe17e92a66bf1
Sourav Kumar Chakravartty <chakravarttysourav@gmail.com>
2024-06-15 12:29:38 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,13 @@ const app = new express();
 
 app.get("/", (req, res) => res.send("<h1>home</h1>"));
 
+
+const heello = (req,res) => {
+    res.send("this is alos a homm page")
+}
+app.get("/hello", heello
+)
+
 const Port = 7456;
 
 app.listen(7456, ()=> console.log(`listening on port number ${Port}`));
